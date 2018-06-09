c665b15c9f572846f9cb5edb7dc02d255b86c5e7
hujiande <593595854@qq.com>
2018-06-09 13:29:48 +0800

    first

---
+++ mianshidemo/src/main/java/com/example/mianshi/demo/DemoMainshiApplication.java
@@ -0,0 +1,12 @@
+package com.example.mianshi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMainshiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMainshiApplication.class, args);
+	}
+}
